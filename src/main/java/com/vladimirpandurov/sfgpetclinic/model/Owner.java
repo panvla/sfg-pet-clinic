dc495670d506380195dc7b30a39b494076a91b1b
panvla <pandvlado@gmail.com>
2022-02-01 19:06:36 +0100

    adding POJOs. Closes #2

---
+++ src/main/java/com/vladimirpandurov/sfgpetclinic/model/Owner.java
@@ -0,0 +1,6 @@
+package com.vladimirpandurov.sfgpetclinic.model;
+
+public class Owner extends Person{
+
+
+}
---
+++ src/main/java/com/vladimirpandurov/sfgpetclinic/model/Vet.java
@@ -0,0 +1,6 @@
+package com.vladimirpandurov.sfgpetclinic.model;
+
+public class Vet extends Person{
+
+
+}
