255037b670699be437eea6908a20349ebcea99b5
panvla <pandvlado@gmail.com>
2022-02-03 22:39:28 +0100

    Impl Map based Services, closes #6

---
+++ pet-clinic-data/src/main/java/com/vladimirpandurov/sfgpetclinic/services/map/VetServiceMap.java
@@ -0,0 +1,33 @@
+package com.vladimirpandurov.sfgpetclinic.services.map;
+
+import com.vladimirpandurov.sfgpetclinic.model.Vet;
+import com.vladimirpandurov.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        super.delete(vet);
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return super.save(vet.getId(), vet);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
