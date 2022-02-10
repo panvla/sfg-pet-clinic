6f8b6d6bc5ca186c0a66da95d418769fdbc28e65
panvla <pandvlado@gmail.com>
2022-02-10 23:09:44 +0100

    added PetTypeService closes #28

---
+++ pet-clinic-data/src/main/java/com/vladimirpandurov/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.vladimirpandurov.sfgpetclinic.services;
+
+import com.vladimirpandurov.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
