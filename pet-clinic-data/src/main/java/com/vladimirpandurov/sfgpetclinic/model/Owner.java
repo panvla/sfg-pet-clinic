c21f3b8bcce4b407645bf4b7d45fc63791b32512
panvla <pandvlado@gmail.com>
2022-02-10 21:28:39 +0100

    added visits , updated owner with pet relationship , closes #25

--- pet-clinic-data/src/main/java/com/vladimirpandurov/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/vladimirpandurov/sfgpetclinic/model/Owner.java
@@ -1,7 +1,12 @@
 package com.vladimirpandurov.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person{
 
+    private Set<Pet> pets;
 
-
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
