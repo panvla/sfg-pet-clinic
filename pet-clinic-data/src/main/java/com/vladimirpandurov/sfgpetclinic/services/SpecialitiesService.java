212adf3a652da1dae6d491c01a3e7fc6da433c94
panvla <pandvlado@gmail.com>
2022-02-11 14:50:06 +0100

    added Specialities Map Impl. closes #30

---
+++ pet-clinic-data/src/main/java/com/vladimirpandurov/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package com.vladimirpandurov.sfgpetclinic.services;
+
+import com.vladimirpandurov.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long>{
+
+}
