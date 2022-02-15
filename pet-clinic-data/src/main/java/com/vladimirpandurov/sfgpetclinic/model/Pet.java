cb52c073966670ea8d807fb704be32cadc3fbb59
panvla <pandvlado@gmail.com>
2022-02-16 00:02:46 +0100

    added visit Entity, closes #37

--- pet-clinic-data/src/main/java/com/vladimirpandurov/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/vladimirpandurov/sfgpetclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.vladimirpandurov.sfgpetclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+
 @Entity
 @Table(name="pets")
 public class Pet extends BaseEntity{
@@ -19,6 +21,17 @@ public class Pet extends BaseEntity{
 
     @Column(name="birth_date")
     private LocalDate birthDate;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy="pet")
+    private Set<Visit> visits = new HashSet<>();
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
 
     public String getName() {
         return name;
