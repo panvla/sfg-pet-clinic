8ad87ace0eb98791a0ce1ab88202e4a525e47647
panvla <pandvlado@gmail.com>
2022-02-15 23:33:25 +0100

    updated vet to use JPA. closes #36

--- pet-clinic-data/src/main/java/com/vladimirpandurov/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/vladimirpandurov/sfgpetclinic/model/Pet.java
@@ -8,12 +8,15 @@ public class Pet extends BaseEntity{
 
     @Column(name="name")
     private String name;
+
     @ManyToOne
     @JoinColumn(name="type_id")
     private PetType petType;
+
     @ManyToOne
     @JoinColumn(name="owner_id")
     private Owner owner;
+
     @Column(name="birth_date")
     private LocalDate birthDate;
 
